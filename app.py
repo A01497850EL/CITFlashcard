c1fafacd7f966aa92102051664b50c592b34c743
AcuSec <MekhaiJS@gmail.com>
2026-04-29 22:17:08 -0700

    edit: changed app.py to peewee

--- app.py
+++ app.py
@@ -1,55 +1,39 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from init_db import init_db, db, Deck, Card
 import random
-from init_db import init_db 
 
 app = Flask(__name__)
 init_db()
 
-def get_db():
-    conn = sqlite3.connect("flashcards.db")
-    conn.row_factory = sqlite3.Row
-    return conn
 
-
-
-#creating decks 
 @app.route("/")
 def index():
-    db = get_db()
-    decks = db.execute("SELECT * FROM decks ORDER BY created_at DESC").fetchall()
-    db.close()
-    return render_template("index.html", decks=decks)
+    return render_template("index.html")
 
+@app.route("/decks")
+def decks():
+    all_decks = Deck.select().order_by(Deck.created_at.desc())
+    return render_template("decks.html", decks=all_decks)
 
-@app.route("/deck/create", methods=["POST"])
+@app.route("/decks/create", methods=["POST"])
 def create_deck():
     name = request.form["name"]
-    db = get_db()
-    db.execute("INSERT INTO decks (name) VALUES (?)", (name,))
-    db.commit()
-    db.close()
-    return redirect(url_for("index"))
+    Deck.create(name=name)
+    return redirect(url_for("decks"))
 
-#creating cards 
-@app.route("/deck/<int:deck_id>")
-def view_deck(deck_id):
-    db = get_db()
-    deck = db.execute("SELECT * FROM decks WHERE id = ?", (deck_id,)).fetchone()
-    cards = db.execute("SELECT * FROM cards WHERE deck_id = ?", (deck_id,)).fetchall()
-    db.close()
+
+@app.route("/decks/<int:id>")
+def view_deck(id):
+    deck  = Deck.get_by_id(id)
+    cards = Card.select().where(Card.deck == id)
     return render_template("deck.html", deck=deck, cards=cards)
 
-@app.route("/deck/<int:deck_id>/card/create", methods=["POST"])
+@app.route("/decks/<int:deck_id>/card/create", methods=["POST"])
 def create_card(deck_id):
     front = request.form["front"]
-    back = request.form["back"]
-    db = get_db()
-    db.execute(
-        "INSERT INTO cards (deck_id, front, back) VALUES (?, ?, ?)",
-        (deck_id, front, back)
-    )
+    back  = request.form["back"]
+    Card.create(deck=deck_id, front=front, back=back)
+    return redirect(url_for("view_deck", id=deck_id))
 
-    db.commit()
-    db.close()
-    return redirect(url_for("view_deck", deck_id=deck_id))
+if __name__ == "__main__":
+    app.run(debug=True)
