4edd0efdb6851a42692802abbc799e6ef8201c81
Enock Lian <qlian@my.bcit.ca>
2026-05-06 09:10:55 -0700

    reworked peewee object structure to allow for tags

--- init_db.py
+++ init_db.py
@@ -7,14 +7,21 @@ class BaseModel(Model):
     class Meta:
         database = db
 
+class DeckTag(BaseModel):
+    name = CharField()
+
 class Deck(BaseModel):
     name = CharField()
     description = CharField(null=True)
-    tags = CharField(null=True)
+    tags = ForeignKeyField(DeckTag, backref="decks", on_delete="CASCADE")
     created_at = DateTimeField(default=datetime.now)
+
+class CardTag(BaseModel):
+    name = CharField()
 
 class Card(BaseModel):
     deck = ForeignKeyField(Deck, backref="cards", on_delete="CASCADE")
+    tag = ForeignKeyField(CardTag, backref="cards", on_delete="CASCADE")
     front = CharField()
     back = CharField()
     mastered = BooleanField(default=False)
